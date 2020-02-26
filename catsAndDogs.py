82300adae5d3e5e397ee6d9ccc845b627a94c144
yash-93 <yashdeepbachhas93@gmail.com>
2020-02-27 01:38:39 +0530

    Update catsAndDogs.py

--- catsAndDogs.py
+++ catsAndDogs.py
@@ -1,47 +1,73 @@
-import cv2
-import numpy as np
+from __future__ import absolute_import, division, print_function, unicode_literals
+import tensorflow as tf
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense, Conv2D, Flatten, Dropout, MaxPooling2D
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
 import os
-import tqdm
-from random import shuffle
+import numpy as np
+import matplotlib as plt
+import matplotlib.pyplot as pyplot
 
-TRAIN_DIR = 'D:/Yashdeep/Kaggle_Datasets/Cats-and-Dogs/train'
-TEST_DIR = 'D:/Yashdeep/Kaggle_Datasets/Cats-and-Dogs/test'
-IMG_SIZE = 50
-LR = 1e-3   # 1 * 10^-3
+_URL = 'https://storage.googleapis.com/mledu-datasets/cats_and_dogs_filtered.zip'
+path_to_zip = tf.keras.utils.get_file('cats_and_dogs.zip', origin=_URL, extract=True)
+PATH = os.path.join(os.path.dirname(path_to_zip), 'cats_and_dogs_filtered')
 
-MODEL_NAME = 'dogsvscats-{}-{}.model'.format(LR, '2conv-basic')
+train_dir = os.path.join(PATH, 'train')
+validation_dir = os.path.join(PATH, 'validation')
+
+train_cats_dir = os.path.join(train_dir, 'cats')  # directory with our training cat pictures
+train_dogs_dir = os.path.join(train_dir, 'dogs')  # directory with our training dog pictures
+validation_cats_dir = os.path.join(validation_dir, 'cats')  # directory with our validation cat pictures
+validation_dogs_dir = os.path.join(validation_dir, 'dogs')  # directory with our validation dog pictures
+
+num_cats_tr = len(os.listdir(train_cats_dir))
+num_dogs_tr = len(os.listdir(train_dogs_dir))
+
+num_cats_val = len(os.listdir(validation_cats_dir))
+num_dogs_val = len(os.listdir(validation_dogs_dir))
+
+total_train = num_cats_tr + num_dogs_tr
+total_val = num_cats_val + num_dogs_val
+
+print('total training cat images:', num_cats_tr)
+print('total training dog images:', num_dogs_tr)
+
+print('total validation cat images:', num_cats_val)
+print('total validation dog images:', num_dogs_val)
+print("--")
+print("Total training images:", total_train)
+print("Total validation images:", total_val)
+
+batch_size = 128
+epochs = 15
+IMG_HEIGHT = 150
+IMG_WIDTH = 150
+
+train_image_generator = ImageDataGenerator(rescale=1./255)
+validation_image_generator = ImageDataGenerator(rescale=1./255)
+
+train_data_gen = train_image_generator.flow_from_directory(batch_size=batch_size,
+                                                           directory=train_dir,
+                                                           shuffle=True,
+                                                           target_size=(IMG_HEIGHT, IMG_WIDTH),
+                                                           class_mode='binary')
+
+val_data_gen = validation_image_generator.flow_from_directory(batch_size=batch_size,
+                                                              directory=validation_dir,
+                                                              target_size=(IMG_HEIGHT, IMG_WIDTH),
+                                                              class_mode='binary')
+
+sample_training_images, _ = next(train_data_gen)
 
 
-def label_img(img):
-    word_label = img.split('.')[-3]
-    if word_label == 'cat': return [1, 0]
-    elif word_label == 'dog': return [0, 1]
+def plotImages(images_arr):
+    fig, axes = pyplot.subplots(1, 5, figsize=(20, 20))
+    axes = (np.array(axes)).flatten()
+    for img, ax in zip(images_arr, axes):
+        ax.imshow(img)
+        ax.axis('off')
+    pyplot.show()
 
 
-def create_train_data():
-    training_data = []
-    for img in tqdm(os.listdir(TRAIN_DIR)):
-        label = label_img(img)
-        path = os.path.join(TRAIN_DIR, img)
-        img = cv2.resize(cv2.imread(path, cv2.IMREAD_GRAYSCALE), (IMG_SIZE, IMG_SIZE))
-        training_data.append([np.array(img), np.array(label)])
-        shuffle(training_data)
-        np.save('train_data.npy', training_data)
-        return training_data
-
-
-def process_test_data():
-    testing_data = []
-    for img in tqdm(os.listdir(TEST_DIR)):
-        path = os.path.join(TEST_DIR, img)
-        img_num = img.split('.')[0]
-        img = cv2.resize(cv2.imread(path, cv2.IMREAD_GRAYSCALE), (IMG_SIZE, IMG_SIZE))
-        testing_data.append([np.array(img), img_num])
-    np.save('test_data.npy', testing_data)
-    return testing_data
-
-
-train_data = create_train_data()
-# if already have train data
-# train_data = np.load('train_data.npy)
+plotImages(sample_training_images[:5])
 
