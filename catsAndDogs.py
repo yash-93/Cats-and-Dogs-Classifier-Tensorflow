f79cea2fcb333a3c03b224cbbd1f0363d6eb20c6
yash-93 <yashdeepbachhas93@gmail.com>
2020-03-01 16:26:04 +0530

    Update catsAndDogs.py
    added 2 more Conv2D layers increases accuracy

--- catsAndDogs.py
+++ catsAndDogs.py
@@ -1,6 +1,10 @@
+# import tensorflow as tf
+# print("Num GPUs Available: ", len(tf.config.experimental.list_physical_devices('GPU')))
+
+from usetrainedmodel import prepare
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Conv2D, MaxPooling2D, Flatten, Dense
-from tensorflow.keras imports optimizers
+from tensorflow.keras import optimizers
 from tensorflow.keras.preprocessing.image import ImageDataGenerator
 
 folder = 'D:/Yashdeep/Kaggle_Datasets/cats_and_dogs/train/'
@@ -10,6 +14,10 @@ dataset_home = 'D:/Yashdeep/Kaggle_Datasets/dataset_dogs_vs_cats/'
 def define_model():
     model = Sequential()
     model.add(Conv2D(32, (3, 3), activation='relu', kernel_initializer='he_uniform', padding='same', input_shape=(200, 200, 3)))
+    model.add(MaxPooling2D((2, 2)))
+    model.add(Conv2D(64, (3, 3), activation='relu', kernel_initializer='he_uniform', padding='same'))
+    model.add(MaxPooling2D((2, 2)))
+    model.add(Conv2D(128, (3, 3), activation='relu', kernel_initializer='he_uniform', padding='same'))
     model.add(MaxPooling2D((2, 2)))
     model.add(Flatten())
     model.add(Dense(128, activation='relu', kernel_initializer='he_uniform'))
